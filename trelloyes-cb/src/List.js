af3905cdadff384a2e2c77d87d0b59eea9b42dbb
Thompson Galetovic <200conceptz@gmail.com>
2019-04-18 17:04:38 -0400

    soon to be draggable

--- trelloyes-cb/src/List.js
+++ trelloyes-cb/src/List.js
@@ -1,31 +1,38 @@
-import React from 'react';
+// import React from 'react';
 import Card from './Card'
 import './List.css';
 import AddItemForm from './AddItemForm'
+import * as React from "react";
+import * as ReactDOM from 'react-dom';
+import Dragula from 'react-dragula';
 
 export default function List(props) {
-  return (
-    <section className='List'>
-      <header className='List-header'>
-        <h2>{props.header}</h2>
-      </header>
-      <div className='List-cards'>
-        {props.cards.map((card) =>
-          <Card
-            listId={props.id}
-            key={card.id}
-            id={card.id}
-            title={card.title}
-            content={card.content}
-            deleteCard={props.deleteCard}
-          />
-        )}
 
-        <section> 
-            <AddItemForm onAddItem= {() => props.addRandom(props.id)} /> 
-        </section>
-
-      </div>
-    </section>
-  )
+    return (
+      <div className='container' ref={this.dragulaDecorator}>>
+      <section className='List'>
+        <header className='List-header'>
+          <h2>{props.header}</h2>
+        </header>
+        <div className='List-cards'>
+          {props.cards.map((card) =>
+            <Card
+              listId={props.id}
+              key={card.id}
+              id={card.id}
+              title={card.title}
+              content={card.content}
+              deleteCard={props.deleteCard}
+            />
+          )}
+  
+          <section> 
+              <AddItemForm onAddItem= {() => props.addRandom(props.id)} /> 
+          </section>
+  
+        </div>
+      </section>
+     </div>
+    )
+  
 }
